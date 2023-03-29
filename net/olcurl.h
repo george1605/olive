5387cc963ea02389d836bb2888deec8581242e54
george1605 <george160505@gmail.com>
2023-03-29 08:47:52 +0300

    added curl write

--- net/olcurl.h
+++ net/olcurl.h
@@ -1,6 +1,12 @@
 #ifndef __OLCURL_H__
 #define __OLCURL_H__
 #include <curl/curl.h>
+#include <string.h>
+
+typedef struct {
+    char* ptr;
+    size_t len;
+} OlChunk;
 
 CURLcode ol_httpget(CURL* curl, const char* link)
 {
@@ -12,11 +18,21 @@ CURLcode ol_httpget(CURL* curl, const char* link)
 
 CURLcode ol_getmail(CURL* curl, char* server, char* name, char* pass)
 {
-    curl_easy_setopt(curl, CURLOPT_USERNAME, "user");
-    curl_easy_setopt(curl, CURLOPT_PASSWORD, "secret");
+    curl_easy_setopt(curl, CURLOPT_USERNAME, name);
+    curl_easy_setopt(curl, CURLOPT_PASSWORD, pass);
  
-    curl_easy_setopt(curl, CURLOPT_URL, "pop3://pop.example.com/1");
+    curl_easy_setopt(curl, CURLOPT_URL, server);
     return curl_easy_perform(curl);
+}
+
+static int ol_writeit(void *ptr, size_t size, size_t nmemb, OlChunk *s)
+{
+    size_t new_len = s->len + size * nmemb;
+    s->ptr = realloc(s->ptr, new_len + 1);
+    memcpy(s->ptr+s->len, ptr, size*nmemb);
+    s->ptr[new_len] = '\0';
+    s->len = new_len;
+    return size * nmemb;
 }
 
 CURL* ol_curlinit(void(*error)())
@@ -33,6 +49,24 @@ CURL* ol_curlinit(void(*error)())
 void ol_curl_cookies(CURL* curl, struct curl_slist *cookies)
 {
     curl_easy_getinfo(curl, CURLINFO_COOKIELIST, &cookies);
+}
+
+void ol_initchunk(OlChunk *s) {
+  s->len = 0;
+  s->ptr = malloc(s->len+1);
+  if (s->ptr == NULL) {
+    fprintf(stderr, "malloc() failed\n");
+    exit(EXIT_FAILURE);
+  }
+  s->ptr[0] = '\0';
+}
+
+OlChunk ol_curl_data(CURL* curl) {
+    OlChunk p;
+    ol_initchunk(&p);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, ol_writeit);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &p);
+    return p;
 }
 
 long long ol_download_size(CURL* curl)
--- net/test.c
+++ net/test.c
@@ -4,6 +4,7 @@ int main()
 {
     CURL* curl = curl_easy_init();
     ol_httpget(curl, "https://www.google.com");
+    OlChunk chunk = ol_curl_data(curl);
+    printf("Data: %i", chunk.len);
     curl_easy_cleanup(curl);
-    printf("Size: %i", ol_download_size(curl));
 }
