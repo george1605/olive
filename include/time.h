272cf26e9cbccb0a944b6accee531a71640e7f27
george1605 <32997165+george1605@users.noreply.github.com>
2023-11-15 14:01:32 +0200

    Added missing endif and ol_wait() function

--- include/time.h
+++ include/time.h
@@ -3,6 +3,11 @@
 #include <time.h>
 #include <string.h>
 #include <stdlib.h>
+#ifdef _WIN32
+#include <windows.h>
+#elif  defined(__linux__)
+#include <unistd.h>
+#endif
 #define OL_INTSEC    0x1
 #define OL_INTMSEC   0x2
 #define OL_INTUSEC   0x4
@@ -29,6 +34,15 @@ int ol_elapsed(OlInterval int, int flags)
   return 1;
 }
 
+void ol_wait(size_t duration)
+{
+#ifdef _WIN32
+  Sleep(duration);
+#elif defined(__linux__) || defined(__UNIX__)
+  sleep(duration);
+#endif
+}
+
 size_t ol_get_year()
 {
   time_t t = time(NULL);
@@ -36,3 +50,4 @@ size_t ol_get_year()
   size_t year = tm.tm_year + 1900;
   return year;
 }
+#endif
