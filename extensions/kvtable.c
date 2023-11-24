754c8a97b7c6b5f0888a69f7d0a631302a51468d
george1605 <32997165+george1605@users.noreply.github.com>
2023-11-24 13:37:59 +0200

    Updated it to fit olive library rules + added ol_drop_kvtable function

--- extensions/kvtable.c
+++ extensions/kvtable.c
@@ -10,6 +10,8 @@
 #define TYPE_UID     0x30 // unsigned long long usually
 #define TYPE_FK      0x40 // pointer
 #define CMD_INSERT   0xFF
+#define CMD_SELECT   0xFE
+#define CMD_DROP     0xFD
 #define E_NOTEXIST   0x20
 #define PRINTF_TYPE(value) _Generic(value, float: "%f", int: "%i", char*: "%s", void*: "%p", default: "%ul")
 
@@ -36,7 +38,13 @@ struct kvtable
     struct kvpair pair;
 };
 
-struct kvtable* new_kvtable()
+struct kvdb
+{
+    char* name; // name of the db
+    struct kvtable* tables; // tables in kv database
+};
+
+struct kvtable* ol_new_kvtable()
 {
     struct kvtable *t = malloc(sizeof(struct kvtable));
     t->prev = NULL;
@@ -45,24 +53,37 @@ struct kvtable* new_kvtable()
     return t;
 }
 
+void ol_drop_kvtable(struct kvtable* table)
+{
+    if(table == NULL) return;
+    struct kvtable* p = table, k;
+    while(p->next)
+    {
+        k = p->next;
+        free(p);
+        p = k;
+    }
+    free(table);
+}
+
 // TO DO expand to other data types
-void print_pair(struct kvpair pair)
+void ol_print_kvpair(struct kvpair pair)
 {
     printf("%i : %i\n", (int)pair.key, (int)pair.value);
 }
 
-void print_table(struct kvtable* table)
+void ol_print_kvtable(struct kvtable* table)
 {
     struct kvtable * p = table;
     while(p != NULL)
     {
-        print_pair(p->pair);
+        ol_print_kvpair(p->pair);
         p = p->next;
     }
 }
 
 // go to the end
-struct kvtable* table_wind(struct kvtable* p)
+struct kvtable* ol_wind_kvtable(struct kvtable* p)
 {
     struct kvtable * q = p;
     while(q->next != NULL)
@@ -70,7 +91,7 @@ struct kvtable* table_wind(struct kvtable* p)
     return q;
 }
 
-struct kvpair make_kvpair(void* key, void* value)
+struct kvpair ol_make_kvpair(void* key, void* value)
 {
     struct kvpair kv;
     kv.key = key;
@@ -81,14 +102,14 @@ struct kvpair make_kvpair(void* key, void* value)
 struct kvtable* insert_kvpair(void* key, void* value, struct kvtable* table)
 {
     struct kvtable* t;
-    t = table_wind(table);
+    t = ol_wind_kvtable(table);
     t->next = (struct kvtable*)malloc(sizeof(struct kvtable));
-    t->next->pair = make_kvpair(key, value);
+    t->next->pair = ol_make_kvpair(key, value);
     t->next->next = NULL; // why i forgot this null?
     return t;
 }
 
-void* search_kv(void* key, struct kvtable* table)
+void* ol_search_kv(void* key, struct kvtable* table)
 {
     struct kvtable* p = table;
     while(p != NULL)
@@ -100,7 +121,7 @@ void* search_kv(void* key, struct kvtable* table)
     return NULL;
 }
 
-void* search_kv_s(char* string, struct kvtable* table)
+void* ol_search_kv_s(char* string, struct kvtable* table)
 {
     struct kvtable* p = table;
     while(p != NULL)
@@ -112,7 +133,7 @@ void* search_kv_s(char* string, struct kvtable* table)
     return NULL;
 }
 
-void write_kv(char* fname, struct kvtable* table)
+void ol_write_kv(char* fname, struct kvtable* table)
 {
     struct kvtable * p = table;
     FILE* fp = fopen(fname, "w");
@@ -124,7 +145,7 @@ void write_kv(char* fname, struct kvtable* table)
     fclose(fp);
 }
 
-struct kvtable* read_kv(char* fname)
+struct kvtable* ol_read_kv(char* fname)
 {
     struct kvtable* i = new_kvtable(), *p;
     p = i;
@@ -143,7 +164,7 @@ struct kvtable* read_kv(char* fname)
 ///    Type System
 /// ///////////////////
 #define PRINTARR(value, type)
-void printarr_kv(void* x, int type)
+void ol_printarr_kv(void* x, int type)
 {
     if(x == NULL) {
         puts("{}");
@@ -153,20 +174,31 @@ void printarr_kv(void* x, int type)
     PRINTARR(x, type);
 }
 
-void makearray_kv(int type, ...)
+void* ol_makearray_kv(int type, int num, ...)
 {
     va_list list;
+    void* x;
     va_start(list, type);
     switch(type)
     {
         case TYPE_NUMBER:
-
-            break;
+            x = malloc(sizeof(float) * num);
+            for(int i = 0;i < num;i++)
+                ((float*)x)[num] = va_arg(list, float);
+        break;
+        default:
+            printf("Unknown type: %i", type);
+        break;
     }
+    va_end(list);
+    return x;
 }
 
+/*
+TEST
 int main()
 {
     printf(PRINTF_TYPE(""), "wow it's good");
     return 0;
 }
+*/
